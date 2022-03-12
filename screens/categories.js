a95707d3da48d6c76de98e3a9b9ca1bca7ac8cb5
Mason Bassett <bassettmason@gmail.com>
2022-03-11 21:02:07 -0800

    categories FlatList recieved from api

--- screens/categories.js
+++ screens/categories.js
@@ -2,20 +2,28 @@ import * as React from 'react';
 import { StyleSheet, Button, View, FlatList, TouchableOpacity, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+
 export default function Categories({ navigation }) {
 
-  const [categories, setCategories] = React.useState([
-    { title: 'ACTOR IN A SUPPORTING ROLE', choice: null, winner: null, key: '1' },
-    { title: 'ACTRESS IN A LEADING ROLE', choice: null, winner: null , key: '2' },
-    { title: 'ACTRESS IN A SUPPORTING ROLE', choice: null, winner: null, key: '3' },
-  ]);
-
+  const [categories, setCategories] = React.useState([]);
+  const getMoviesFromApiAsync = async () => {
+    try {
+      const response = await fetch(
+        'https://Oscars-Pool-App.masonp00pyd.repl.co//api/getcategorylist'
+      );
+      const json = await response.json();
+      return json.categories;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  setCategories(getMoviesFromApiAsync());
   return (
     <View style={globalStyles.container}>
       <FlatList data={categories} renderItem={({ item }) => (
         <TouchableOpacity onPress={() => navigation.navigate('Nominees', item)}>
           <Card>
-            <Text style={globalStyles.titleText}>{ item.title }</Text>
+            <Text style={globalStyles.titleText}>{ item.category }</Text>
           </Card>
         </TouchableOpacity>
       )} />
